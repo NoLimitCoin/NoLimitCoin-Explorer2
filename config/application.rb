78ef6f4554a9f455b5cf96cb8f4770bc6ee4c86d
Sambhav Sharma <sambhav.sharma@hotmail.com>
2017-12-15 02:13:17 +0530

    adding lin in eager load paths

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,6 @@ module NolimitcoinExplorer2
   class Application < Rails::Application
 
     config.autoload_paths += Dir[Rails.root.join('lib'), Rails.root.join('app')]
+    config.eager_load_paths += Dir[Rails.root.join('lib')]
   end
 end
